e569acb35a949dfcb21f502936579608bc26c69a
charlieTai <charlie.tai@tpisoftware.com>
2025-07-12 20:29:35 +0800

    add : 建立滾動區塊

---
+++ layouts/scrollSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <section
+      ref="sectionRef"
+      class="h-screen snap-start flex items-center justify-center">
+    <slot name="title" />
+    <slot name="content" />
+    <slot name="footer" />
+  </section>
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const sectionRef = ref<HTMLElement | null>(null)
+
+defineExpose({
+  scrollIntoView: () => {
+    sectionRef.value?.scrollIntoView({ behavior: 'smooth' })
+  }
+})
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,30 +2,37 @@
   <!-- 可滾動的內容區塊 -->
     <div class="scrollable-content-wrapper h-screen overflow-hidden">
       <div class="snap-y snap-mandatory h-full overflow-y-scroll scroll-smooth pt-[60px]"><!-- 加 padding 避免蓋住第一塊 -->
-        <section
-            ref="section1"
-            class="h-screen snap-start flex items-center justify-center"
+        <ScrollSection ref="initSection" :class="['flex-col gap-[20px]']">
+          <template #content>
+            <h1 class="text-4xl">Charlie Tai</h1>
+            <p class="text-2xl text-gray-500">研發工程師 / 前端開發</p>
+            <CButton
+                :label="'了解更多'"
+                :size="'lg'"
+                :shadow="true"
+                @click="handlerClickButton()"
+            />
+          </template>
+        </ScrollSection>
+        <!--關於我-區塊-->
+        <ScrollSection
+            ref="aboutSection"
             :class="['flex-col gap-[20px]']"
         >
-          <h1 class="text-4xl">Charlie Tai</h1>
-          <p class="text-2xl text-gray-500">研發工程師 / 前端開發</p>
-          <CButton
-              :label="'了解更多'"
-              :size="'lg'"
-              :shadow="true"
-              @click="handlerClickButton()"
-          />
-        </section>
-        <section
-            ref="section2"
-            class="h-screen snap-start bg-green-100 flex items-center justify-center">
-          <h1 class="text-4xl">區塊 2</h1>
-        </section>
-        <section
-            ref="section3"
-            class="h-screen snap-start bg-yellow-100 flex items-center justify-center">
-          <h1 class="text-4xl">區塊 3</h1>
-        </section>
+          <template #title
+                    :class="['flex w-full py-[3rem]']">
+            <h1 class="text-4xl">關於我</h1>
+          </template>
+          <template #content>
+            <h1 class="text-4xl">關於我-區塊</h1>
+          </template>
+        </ScrollSection>
+        <!--經驗-區塊-->
+        <ScrollSection ref="experienceSection">
+          <template #content>
+            <h1 class="text-4xl">經驗-區塊</h1>
+          </template>
+        </ScrollSection>
       </div>
     </div>
 </template>
@@ -33,11 +40,18 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import {CButton} from "#components";
+import ScrollSection from "~/layouts/scrollSection.vue";
 
-const section2 = ref<HTMLElement | null>(null)
+const section2 = ref<HTMLElement | null>(null);
+/** 初始化Section*/
+const initSection = ref();
+/** 關於我Section*/
+const aboutSection = ref()
+/** 經歷Section*/
+const experienceSection = ref()
 
 const handlerClickButton = () =>{
-  section2.value?.scrollIntoView({ behavior: 'smooth' })
+  aboutSection.value?.scrollIntoView({ behavior: 'smooth' })
 }
 </script>
 
