59106007e90015955cd40494567c0bedba999d49
charlieTai <charlie.tai@tpisoftware.com>
2025-07-13 23:17:00 +0800

    add : 實現關於我頁面

--- pages/index.vue
+++ pages/index.vue
@@ -17,14 +17,17 @@
         <!--關於我-區塊-->
         <ScrollSection
             ref="aboutSection"
-            :class="['flex-col gap-[20px]']"
+            :class="['flex-col gap-[20px] !justify-start']"
         >
-          <template #title
-                    :class="['flex w-full py-[3rem]']">
-            <h1 class="text-4xl">關於我</h1>
+          <template #title>
+            <div :class="['flex w-full items-center justify-center']">
+              <p class="underline-title">關於我</p>
+            </div>
           </template>
           <template #content>
-            <h1 class="text-4xl">關於我-區塊</h1>
+            <div class="about-section-content mx-auto max-w-[60%] text-justify py-[40px] text-[15px] leading-[2] drop-cap-container">
+              因興趣而接觸前端開發，自學過程中逐漸體會到成就感與樂趣，進而立志成為專職工程師。工作上，曾以 Angular 為主要前端框架，參與多項產品與專案開發。因應公司策略調整，目前轉向 Vue 生態系，持續學習 Vue 與 Nuxt 以深化實戰能力。在自學路上從未間斷，進一步學習後端技術，如 Node.js 與 NestJS，從中理解前後端的整體架構與資料流處理。此外亦掌握 Docker，並將容器化技術應用於實際開發與部署環境中，提升開發效率與穩定性。曾參與專案包含《原住民族住宅業務系統》、《原住民族知識體系》等政府平台開發，亦參與產品開發如《Systalk Chat》。閒暇之餘亦會撰寫 side project，嘗試將其中元件獨立封裝為可重用套件並發布至 npm，持續累積技術深度與開源實作經驗。
+            </div>
           </template>
         </ScrollSection>
         <!--經驗-區塊-->
